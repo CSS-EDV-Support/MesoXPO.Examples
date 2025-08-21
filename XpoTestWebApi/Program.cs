1951fdc18db0e873e85b13110cc102bd691de18f
Tobias Forbrich <tf@css-edv-support.de>
2025-08-21 14:56:22 +0200

    Aktualisiere .NET in WebApi

--- XpoTestWebApi/Program.cs
+++ XpoTestWebApi/Program.cs
@@ -7,7 +7,6 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 #region XPO Datenzugriff
 
 var connectionString = builder.Configuration.GetConnectionString("WinLineConnectionString");
@@ -21,11 +20,6 @@ builder.Services.AddXpoDefaultDataLayer(ServiceLifetime.Scoped,
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
 
 app.UseHttpsRedirection();
 
